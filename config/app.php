f5c5dc4d398fedb4eef73ea16fc7210e4b4784c3
alvibd <alviahmed811@gmail.com>
2017-10-25 11:23:24 +0600

    added debugbar and pretty routes

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
+        PrettyRoutes\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +227,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * 3rd party
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
