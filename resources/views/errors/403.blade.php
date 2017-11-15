00f28eab3dc13cc563a71d55d501c9da9ccbdbed
alvibd <alviahmed811@gmail.com>
2017-11-15 18:15:10 +0600

    authorization error page added

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.base')
+
+@section('page_title', "FANPLAYoff | 403")
+
+@section('content')
+    <!-- HERO AREA START -->
+    <div class="hero-area padding-bottom-less padding-top-less hero-area-bg">
+
+    </div>
+    <!-- HERO AREA END -->
+
+    <!-- BLANK AREA START -->
+    <div class="join-league">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="{{ asset('img/403.png') }}" class='img-responsive'>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
